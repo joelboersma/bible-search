13939cbf8ad7bb58b71517b0e40cbc75fd7a719a
Joel Boersma <joelboersma@gmail.com>
2023-03-18 16:33:06 -0700

    fix SearchBar border

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -8,17 +8,17 @@ const text = ref("");
 </script>
 
 <template>
-  <div
-    class="border border-solid px-4 py-2 mb-2 rounded-full border-gray-400 w-full text-lg flex items-center"
-  >
+  <div class="flex relative">
     <input
       v-model="text"
       name="search"
       type="search"
       :placeholder="props.placeholder"
-      class="w-full"
+      class="border border-solid px-4 py-2 mb-2 rounded-full border-gray-400 w-full text-lg flex items-center relative"
     />
-    <MagnifyingGlassIcon class="w-6 h-6 stroke-gray-600" />
+    <MagnifyingGlassIcon
+      class="w-6 h-6 stroke-gray-600 absolute right-4 top-2.5"
+    />
   </div>
 </template>
 
