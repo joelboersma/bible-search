3fd694558eb5d45350b9680116320cf0f6630330
Joel Boersma <joelboersma@gmail.com>
2023-03-18 15:39:58 -0700

    search icon in SearchBar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,16 +1,25 @@
 <script setup>
-import { ref } from "vue";
+import { ref, defineProps } from "vue";
+import { MagnifyingGlassIcon } from "@heroicons/vue/24/outline";
+
+const props = defineProps(["placeholder"]);
 
 const text = ref("");
 </script>
 
 <template>
-  <input
-    v-model="text"
-    name="search"
-    type="search"
-    class="border border-solid px-4 py-2 mb-2 rounded-full border-gray-400 w-full text-lg"
-  />
+  <div
+    class="border border-solid px-4 py-2 mb-2 rounded-full border-gray-400 w-full text-lg flex items-center"
+  >
+    <input
+      v-model="text"
+      name="search"
+      type="search"
+      :placeholder="props.placeholder"
+      class="w-full"
+    />
+    <MagnifyingGlassIcon class="w-6 h-6 stroke-gray-600" />
+  </div>
 </template>
 
 <script></script>
