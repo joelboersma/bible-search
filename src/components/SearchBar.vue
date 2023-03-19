9832caefb1ee01d38f2917c120daea93c222c007
Joel Boersma <joelboersma@gmail.com>
2023-03-18 17:29:48 -0700

    add submit button

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -14,7 +14,7 @@ defineEmits(["update:modelValue"]);
       @input="$emit('update:modelValue', $event.target.value)"
       name="search"
       type="search"
-      class="border border-solid px-4 py-2 mb-2 rounded-full border-gray-400 w-full text-lg flex items-center relative"
+      class="border border-solid px-4 py-2 rounded-full border-gray-400 w-full text-lg flex items-center relative"
     />
     <MagnifyingGlassIcon
       class="w-6 h-6 stroke-gray-600 absolute right-4 top-2.5"
--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -3,15 +3,29 @@ import { ref } from "vue";
 import SearchBar from "../components/SearchBar.vue";
 
 const searchQuery = ref("");
+
+function handleSubmit(e) {
+  console.log(searchQuery.value);
+}
 </script>
 
 <template>
   <div class="flex justify-center w-full">
-    <SearchBar
-      v-model="searchQuery"
-      placeholder="Search verses by keyword"
-      class="w-full max-w-xl"
-    ></SearchBar>
+    <form
+      @submit.prevent="handleSubmit"
+      class="flex justify-center w-full max-w-xl flex-wrap sm:flex-nowrap"
+    >
+      <SearchBar
+        v-model="searchQuery"
+        placeholder="Search verses by keyword"
+        class="w-full"
+      ></SearchBar>
+      <button
+        type="submit"
+        class="bg-emerald-800 hover:bg-emerald-700 transition-colors text-white px-4 py-2 rounded-lg ml-0 mt-2 w-full sm:w-auto sm:ml-2 sm:my-0"
+      >
+        Submit
+      </button>
+    </form>
   </div>
-  <p>{{ searchQuery }}</p>
 </template>
