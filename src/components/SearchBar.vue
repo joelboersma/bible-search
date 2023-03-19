1cc9df2664d1da135b9ff21c534b2446e51bb453
Joel Boersma <joelboersma@gmail.com>
2023-03-18 17:01:14 -0700

    fix v-model for SearchBar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,19 +1,19 @@
 <script setup>
-import { ref, defineProps } from "vue";
+import { defineProps, defineEmits } from "vue";
 import { MagnifyingGlassIcon } from "@heroicons/vue/24/outline";
 
-const props = defineProps(["placeholder"]);
-
-const text = ref("");
+defineProps(["modelValue", "placeholder"]);
+defineEmits(["update:modelValue"]);
 </script>
 
 <template>
   <div class="flex relative">
     <input
-      v-model="text"
+      :value="modelValue"
+      :placeholder="placeholder"
+      @input="$emit('update:modelValue', $event.target.value)"
       name="search"
       type="search"
-      :placeholder="props.placeholder"
       class="border border-solid px-4 py-2 mb-2 rounded-full border-gray-400 w-full text-lg flex items-center relative"
     />
     <MagnifyingGlassIcon
