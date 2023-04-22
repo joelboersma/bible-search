b95a4f0bb237c938b97ec3785187724b974b20b3
Joel Boersma <joelboersma@gmail.com>
2023-04-22 12:57:02 -0700

    add more variables from search response

--- src/components/SearchResultsTable.vue
+++ src/components/SearchResultsTable.vue
@@ -3,31 +3,47 @@ import { ref, watch } from "vue";
 import { getSearch } from "../services/esvApi";
 
 const fetchedResults = ref(false);
+const foundResults = ref(false);
 const results = ref([]);
-// const totalResults = ref(0);
-// const currentPage = ref(0);
-// const totalPages = ref(0);
+const totalResults = ref(0);
+const currentPage = ref(0);
+const totalPages = ref(0);
 
 const props = defineProps(["query"]);
 
 watch(
   () => props.query,
   (newQuery) => {
+    reset();
     fetchFirstPageSearchResults(newQuery);
   }
 );
 
-async function fetchFirstPageSearchResults(searchTerm) {
+function reset() {
   fetchedResults.value = false;
+  foundResults.value = false;
+  results.value = [];
+  totalResults.value = 0;
+  currentPage.value = 0;
+  totalPages.value = 0;
+}
+
+async function fetchFirstPageSearchResults(searchTerm) {
   const response = await getSearch(searchTerm);
   fetchedResults.value = true;
   console.log(response);
-  results.value = response.results;
+  if (response?.results?.length > 0) {
+    foundResults.value = true;
+    results.value.push(response.results);
+    totalResults.value = response.total_results;
+    currentPage.value = response.page;
+    totalPages.value = response.total_pages;
+  }
 }
 </script>
 
 <template>
-  <table v-if="results?.length > 0" class="my-4">
+  <table v-if="foundResults" class="my-4">
     <thead>
       <tr>
         <th>Reference</th>
@@ -35,7 +51,7 @@ async function fetchFirstPageSearchResults(searchTerm) {
       </tr>
     </thead>
     <tbody>
-      <tr v-for="result in results">
+      <tr v-for="result in results[0]">
         <td>{{ result.reference }}</td>
         <td>{{ result.content }}</td>
       </tr>
