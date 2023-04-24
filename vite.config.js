07967d62ec0ed7296eb3587d7b8971d2714f1731
Joel Boersma <joelboersma@gmail.com>
2023-04-23 17:50:17 -0700

    open browser if BROWSER environment variable is set

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  loadEnv(mode, process.cwd(), "");
+  return {
+    // vite config
+    plugins: [vue()],
+    server: {
+      open: Boolean(process.env.BROWSER),
+    },
+  };
 });
