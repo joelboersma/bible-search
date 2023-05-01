91ac3cff8746c30b764b149d1eeb3fb001267857
Joel Boersma <joelboersma@gmail.com>
2023-04-30 18:58:17 -0700

    SearchBar: fix outline offset

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -14,7 +14,7 @@ defineEmits(["update:modelValue"]);
       name="search"
       type="search"
       autocomplete="off"
-      class="border border-solid px-4 py-2 rounded-full border-gray-400 w-full text-lg flex items-center relative"
+      class="border border-solid px-4 py-2 rounded-full border-gray-400 w-full text-lg flex items-center relative outline-offset-0"
     />
     <MagnifyingGlassIcon
       class="w-6 h-6 stroke-gray-600 absolute right-4 top-2.5"
