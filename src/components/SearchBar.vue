a0da4653aa84ed64d7edb13353e823c4d4275e5c
Joel Boersma <joelboersma@gmail.com>
2023-05-01 18:49:37 -0700

    use type='text' because outlines suck

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -12,9 +12,12 @@ defineEmits(["update:modelValue"]);
       :placeholder="placeholder"
       @input="$emit('update:modelValue', $event.target.value)"
       name="search"
-      type="search"
+      type="text"
+      inputmode="search"
+      aria-label="search"
+      role="searchbox"
       autocomplete="off"
-      class="border border-solid px-4 py-2 rounded-full border-gray-400 w-full text-lg flex items-center relative outline-offset-0"
+      class="border border-solid px-4 py-2 rounded-full border-gray-400 w-full text-lg flex items-center relative"
     />
     <MagnifyingGlassIcon
       class="w-6 h-6 stroke-gray-600 absolute right-4 top-2.5"
@@ -23,20 +26,3 @@ defineEmits(["update:modelValue"]);
 </template>
 
 <script></script>
-
-<style scoped>
-/**
-On Webkit (Chromium + Safari) an 'X' icon will appear in 
-'input[type="search"]' for clearing it. This is non-standard so 
-it will be hidden.
-Eventually, this can be implemented manually.
-*/
-
-/* Webkit (Chromium + Safari) */
-input[type="search"]::-webkit-search-decoration,
-input[type="search"]::-webkit-search-cancel-button,
-input[type="search"]::-webkit-search-results-button,
-input[type="search"]::-webkit-search-results-decoration {
-  display: none;
-}
-</style>
