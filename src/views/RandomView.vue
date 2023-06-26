fb90859cbaa49d9da4e21a74ba5e92aa16dcea99
Joel Boersma <joelboersma@gmail.com>
2023-06-25 18:27:36 -0700

    style the random view

--- src/views/RandomView.vue
+++ src/views/RandomView.vue
@@ -14,7 +14,14 @@ async function fetchRandomVerse() {
 </script>
 
 <template>
-  <button :onclick="fetchRandomVerse">Get Random Verse!</button>
-  <h2>{{ verse }}</h2>
-  <p v-if="text">{{ text }}</p>
+  <button
+    class="bg-emerald-800 hover:bg-emerald-700 transition-colors text-white text-lg px-4 py-2 rounded-lg w-full"
+    :onclick="fetchRandomVerse"
+  >
+    Get Random Verse!
+  </button>
+  <section v-if="verse" class="mt-6">
+    <h2 class="font-serif text-xl mb-2">{{ verse }}</h2>
+    <p v-if="text">{{ text }}</p>
+  </section>
 </template>
