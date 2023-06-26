071072790871d027b89d08ade823f9d6462fcafd
Joel Boersma <joelboersma@gmail.com>
2023-06-25 18:07:05 -0700

    display results on page

--- src/views/RandomView.vue
+++ src/views/RandomView.vue
@@ -1,12 +1,20 @@
 <script setup>
+import { ref } from "vue";
 import { getText } from "../services/esvApi";
+
+const verse = ref("");
+const text = ref("");
 
 async function fetchRandomVerse() {
   const result = await getText("John 3:16");
   console.log(result);
+  verse.value = result.canonical;
+  text.value = result.passages[0];
 }
 </script>
 
 <template>
   <button :onclick="fetchRandomVerse">Get Random Verse!</button>
+  <h2>{{ verse }}</h2>
+  <p v-if="text">{{ text }}</p>
 </template>
