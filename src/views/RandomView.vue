dd0fd35f0202403d0425735d8822484a5d96d58b
Joel Boersma <joelboersma@gmail.com>
2023-06-25 17:59:58 -0700

    use getText for John 3:16

--- src/views/RandomView.vue
+++ src/views/RandomView.vue
@@ -1,1 +1,12 @@
-<template>Random View!</template>
+<script setup>
+import { getText } from "../services/esvApi";
+
+async function fetchRandomVerse() {
+  const result = await getText("John 3:16");
+  console.log(result);
+}
+</script>
+
+<template>
+  <button :onclick="fetchRandomVerse">Get Random Verse!</button>
+</template>
