3c6860437e358bce17592da62e9656c0f8d018a9
Joel Boersma <joelboersma@gmail.com>
2023-09-16 21:00:30 -0700

    update search bar for dark mode

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -17,7 +17,7 @@ defineEmits(["update:modelValue"]);
       aria-label="search"
       role="searchbox"
       autocomplete="off"
-      class="border border-solid px-4 py-2 rounded-full border-gray-400 w-full text-lg flex items-center relative"
+      class="border border-solid px-4 py-2 rounded-full border-gray-400 w-full text-lg flex items-center relative dark:bg-neutral-800"
     />
     <MagnifyingGlassIcon
       class="w-6 h-6 stroke-gray-600 absolute right-4 top-2.5"
