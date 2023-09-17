078fd829ff1b8c9ed78058c9df386d64ca2e7d5c
Joel Boersma <joelboersma@gmail.com>
2023-09-16 20:54:18 -0700

    top-level dark styles

--- src/App.vue
+++ src/App.vue
@@ -36,7 +36,7 @@ const currentView = computed(() => {
   <main>
     <div class="flex justify-center">
       <div
-        class="w-full max-w-screen-lg p-6 sm:border sm:border-solid sm:border-gray-200 sm:p-8 sm:my-4 sm:mx-8 sm:rounded-lg sm:shadow-md"
+        class="w-full max-w-screen-lg p-6 sm:border sm:border-solid sm:border-neutrals-200 dark:sm:border-neutral-700 sm:p-8 sm:my-4 sm:mx-8 sm:rounded-lg sm:shadow-md"
       >
         <component :is="currentView" />
       </div>
