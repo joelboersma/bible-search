6d726d546c71b05bc42e7c892833fd213f5b5039
Joel Boersma <joelboersma@gmail.com>
2023-09-16 21:27:28 -0700

    update AboutView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,14 +1,22 @@
 <template>
-  <p>
-    Unless otherwise indicated, all Scripture quotations are from the ESV® Bible
-    (The Holy Bible, English Standard Version®), © 2001 by Crossway, a
-    publishing ministry of Good News Publishers. Used by permission. All rights
-    reserved. The ESV text may not be quoted in any publication made available
-    to the public by a Creative Commons license. The ESV may not be translated
-    into any other language.
-  </p>
-  <p>
-    Users may not copy or download more than 500 verses of the ESV Bible or more
-    than one half of any book of the ESV Bible.
-  </p>
+  <div class="flex justify-center">
+    <article class="max-w-xl">
+      <p class="mb-2">
+        All scriptures retrieved from the
+        <a href="https://api.esv.org/" class="text-blue-600">ESV API</a>.
+      </p>
+      <p class="mb-2">
+        Scripture quotations are from the ESV® Bible (The Holy Bible, English
+        Standard Version®), © 2001 by Crossway, a publishing ministry of Good
+        News Publishers. Used by permission. All rights reserved. The ESV text
+        may not be quoted in any publication made available to the public by a
+        Creative Commons license. The ESV may not be translated into any other
+        language.
+      </p>
+      <p>
+        Users may not copy or download more than 500 verses of the ESV Bible or
+        more than one half of any book of the ESV Bible.
+      </p>
+    </article>
+  </div>
 </template>
